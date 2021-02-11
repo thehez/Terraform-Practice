da49cfd9893606a83c5bc835092db1e1b781583d
Christian <christianheselton@gmail.com>
2021-02-11 16:03:52 -0500

    Added Variables

--- main.tf
+++ main.tf
@@ -2,10 +2,33 @@ provider "aws" {
  region = "us-east-2"
 }
 
+variable "server_port" {
+ description = "The port the server will use for HTTP requests"
+ type = number
+ default = 8080
+}
+
+resource "aws_security_group" "instance" {
+ name = "terraform-example-instance"
+ ingress {
+    from_port = var.server_port 
+    to_port = var.server_port
+    protocol = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+ }
+}
+
 resource "aws_instance" "example" {
  ami = "ami-0c55b159cbfafe1f0"
  instance_type = "t2.micro"
+ vpc_security_group_ids = [aws_security_group.instance.id]
  
+ user_data = <<-EOF
+ #!/bin/bash
+ echo "Hello, World" > index.html
+ nohup busybox httpd -f -p 8080 &
+ EOF
+
  tags = {
    Name = "terraform-example"
  }
